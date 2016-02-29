900384b1a8a6ed44ccff82bccbab63cca5feb011
charltonaustin <charlton.austin@gmail.com>
2016-02-28 20:54:09 -0500

    CA|PH: Adding in basic diff.

---
+++ src/main/java/com/charltonaustin/SemanticDiff.java
@@ -0,0 +1,70 @@
+package com.charltonaustin;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseException;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SemanticDiff {
+
+
+    public boolean itIsTheSame(String from, String to) {
+        HashSet<String> myMethods = new HashSet<String>();
+
+        giveMeMethods(from, myMethods);
+
+        HashSet<String> myOtherMethods = new HashSet<String>();
+
+        giveMeMethods(to, myOtherMethods);
+        return Sets.symmetricDifference(myMethods, myOtherMethods).isEmpty();
+    }
+
+    private void giveMeMethods(String src, HashSet<String> myMethods) {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(src.getBytes());
+
+        CompilationUnit cu;
+        try {
+            // parse the file
+            cu = JavaParser.parse(inputStream);
+            MethodVisitor methodVisitor = new MethodVisitor(myMethods);
+            methodVisitor.visit(cu, null);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * Simple visitor implementation for visiting MethodDeclaration nodes.
+     */
+    private static class MethodVisitor extends VoidVisitorAdapter {
+
+        Set<String> myMethods;
+
+        public MethodVisitor(Set<String> myMethods) {
+            this.myMethods = myMethods;
+        }
+
+        @Override
+        public void visit(MethodDeclaration n, Object arg) {
+
+            String signature = n.getName();
+            signature = signature + " " + n.getParameters();
+            myMethods.add(signature );
+            super.visit(n, arg);
+        }
+    }
+}
---
+++ src/test/java/com/charltonaustin/AppTest.java
@@ -0,0 +1,56 @@
+package com.charltonaustin;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseException;
+import com.google.common.collect.Sets;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    private SemanticDiff semanticDiff;
+
+    @Before
+    public void setUp() throws Exception {
+        semanticDiff = new SemanticDiff();
+    }
+
+    @Test
+    public void testMethodMoveIsNotDiff() {
+        String from = "public class Something {public void hello(){}\n" +
+                "public void goodbye(){}}\n";
+        String to = "public class Something {public void goodbye(){}\n" +
+                "public void hello(){}}\n";
+
+        assertTrue(semanticDiff.itIsTheSame(from, to));
+    }
+
+    @Test
+    public void testAddingMethodIsDifferent() {
+        String from = "public class Something {public void hello(){}\n" +
+                "public void goodbye(){}}\n";
+        String to = "public class Something {public void goodbye(){}\n" +
+                "public void hello(){}" +
+                "public void different(){}}\n";
+
+        assertFalse(semanticDiff.itIsTheSame(from, to));
+    }
+
+    @Test
+    public void testAddingMethodParameterIsDifferent() {
+        String from = "public class Something {public void hello(int a){}}\n";
+        String to = "public class Something {public void hello(){}}\n";
+
+        assertFalse(semanticDiff.itIsTheSame(from, to));
+    }
+
+
+}
